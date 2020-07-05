ef8b57428019b9cf71c6da503909a7c3c7c82273
Hussam Eldin Mohamed <dr.hussam@live.com>
2020-07-05 01:06:34 -0400

    TikTokManger static class created , and system IO used.

--- Assignment_02/Program.cs
+++ Assignment_02/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Assignment_02
 {
@@ -83,4 +84,87 @@ namespace Assignment_02
             return tikTok;
         }
     }
+
+    static class TikTokManger
+    {
+        private static List<TikTok> TIKTOKS;
+        private static string FILENAME = "tikTok.txt";
+
+        static TikTokManger()
+        {
+            TIKTOKS = new List<TikTok>();
+            TextReader reader = new StreamReader(FILENAME);
+
+            int linesCount = File.ReadAllLines(FILENAME).Length;
+            for (int i = 0; i < linesCount; i++)
+            {
+                string line = reader.ReadLine();
+                if (line != null)
+                {
+                    TIKTOKS.Add(TikTok.Prase(line));
+                }
+
+            }
+        }
+
+        public static void Intializer()
+        {
+            TikTok tiktok1 = new TikTok("Lora", 4, "Note10", Audience.World);
+            TikTok tiktok2 = new TikTok("james", 29, "SunSet", Audience.Special);
+            TikTok tikTok3 = new TikTok("Rogy", 19, "Funny", Audience.Special);
+            TikTok tikTok4 = new TikTok("Qusai", 19, "Playstation", Audience.Group);
+            TikTok tikTok5 = new TikTok("Sara", 28, "Centennial", Audience.World);
+            TIKTOKS.Add(tiktok1);
+            TIKTOKS.Add(tiktok2);
+            TIKTOKS.Add(tikTok3);
+            TIKTOKS.Add(tikTok4);
+            TIKTOKS.Add(tikTok5);
+        }
+
+        public static void Show()
+        {
+            foreach (var tiktok in TIKTOKS)
+            {
+                Console.WriteLine(tiktok.ToString());
+            }
+        }
+
+        public static void Show(string tag)
+        {
+            foreach (var tiktok in TIKTOKS)
+            {
+                if (tiktok.HashTag.ToUpper() == tag)
+                {
+                    Console.WriteLine(tiktok);
+                }
+            }
+        }
+
+        public static void Show(int length)
+        {
+            foreach (var tiktok in TIKTOKS)
+            {
+                if (tiktok.Length == length)
+                {
+                    Console.WriteLine(tiktok);
+                }
+            }
+        }
+
+        public static void Show(Audience audience)
+        {
+            foreach (var tiktok in TIKTOKS)
+            {
+                if (tiktok.Audience == audience)
+                {
+                    Console.WriteLine(tiktok);
+                }
+            }
+
+        }
+    }
+}
+
+
+
 }
